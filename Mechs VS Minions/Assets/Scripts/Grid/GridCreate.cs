160b83e083186c2881a5c4a2673abb155e77cf9d
BuildTools <unconfigured@null.spigotmc.org>
2018-04-23 19:37:39 +0200

    Grid Manager V2 (multiple grids)

--- Mechs VS Minions/Assets/Scripts/Grid/GridCreate.cs
+++ Mechs VS Minions/Assets/Scripts/Grid/GridCreate.cs
@@ -4,32 +4,35 @@ public class GridCreate : MonoBehaviour {
 
     [SerializeField] private int _gridWidth = 5;
     [SerializeField] private int _gridHeight = 5;
+    [SerializeField] private GameObject _tile;
 	
     public void GenerateGrid()
     {
-        RemoveGrid();
+        //RemoveGrid();
         CreateGrid();
     }
 
 	private void CreateGrid()
     {
+        GameObject parent = new GameObject("Grid");
+
         for (int i = 0; i < _gridWidth; i++)
         {
             for (int j = 0; j < _gridHeight; j++)
             {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.parent = gameObject.transform;
-                cube.transform.position = new Vector3(i, 0, j);
+                GameObject tile = Instantiate(_tile, parent.transform);
+                tile.name = "Tile [" + i + "," + j + "]";
+                tile.transform.position = new Vector3(i, 0, j);
             }
         }
     }
 
-    private void RemoveGrid()
+    /*private void RemoveGrid()
     {
         int childCount = transform.childCount;
         for (int i = 0; i < childCount; i++)
         {
             DestroyImmediate(transform.GetChild(0).gameObject);
         }
-    }
+    }*/
 }

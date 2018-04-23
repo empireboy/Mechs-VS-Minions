b34f0b30f1560fd3205c99c2d335362696ea5c01
BuildTools <unconfigured@null.spigotmc.org>
2018-04-23 19:09:44 +0200

    Grid Manager V1

---
+++ Mechs VS Minions/Assets/Scripts/Grid/GridCreate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GridCreate : MonoBehaviour {
+
+    [SerializeField] private int _gridWidth = 5;
+    [SerializeField] private int _gridHeight = 5;
+	
+    public void GenerateGrid()
+    {
+        RemoveGrid();
+        CreateGrid();
+    }
+
+	private void CreateGrid()
+    {
+        for (int i = 0; i < _gridWidth; i++)
+        {
+            for (int j = 0; j < _gridHeight; j++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.parent = gameObject.transform;
+                cube.transform.position = new Vector3(i, 0, j);
+            }
+        }
+    }
+
+    private void RemoveGrid()
+    {
+        int childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            DestroyImmediate(transform.GetChild(0).gameObject);
+        }
+    }
+}
---
+++ Mechs VS Minions/Assets/Scripts/Grid/GridCreateEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridCreate))]
+public class GridCreateEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GridCreate createGridScript = (GridCreate)target;
+        if (GUILayout.Button("Generate Grid"))
+        {
+            createGridScript.GenerateGrid();
+        }
+    }
+}
